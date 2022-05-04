e78631963ac328df7e64d3849130db1608409aaa
Matt Eilar <meilar@gmail.com>
2022-05-04 11:44:45 -0700

    Add firebase signin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 function Signin(){
+
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+      console.log("Successfully Signed Up");
+    }).catch((error)=> {console.log(error.message)});
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.email.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(()=> {
+      console.log("Successfully signed in");
+    }).catch((error)=> {
+      console.log(error.message)
+    });
+  }
+
+  function doSignOut(){
+    firebase.auth().signOut().then(()=> {
+      console.log("Signed Out!");
+    }).catch((error) =>
+      console.log(error.message));
+  }
+
   return(
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign up</h1>
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+      <h1>Sign In</h1>
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign out</button>
+    </React.Fragment>
   )
 }
 
